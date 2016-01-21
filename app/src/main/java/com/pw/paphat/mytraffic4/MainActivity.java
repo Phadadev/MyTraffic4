7f6fbbbd895b953051e6b3bd6b477383a81b50a3
NYR <paphat.dev@gmail.com>
2016-01-21 15:48:40 +0700

    Link MainAct to ShowDetailAct

--- app/src/main/java/com/pw/paphat/mytraffic4/MainActivity.java
+++ app/src/main/java/com/pw/paphat/mytraffic4/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
     private void createListView() {
         MyData objMyData = new MyData();
         int[] intIcon = objMyData.icon();
-        String[] strTitle = objMyData.title();
+        final String[] strTitle = objMyData.title();
 
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this,intIcon,strTitle);
         ListView myListView = (ListView) findViewById(R.id.listView);
         myListView.setAdapter(objMyAdapter);
+
+        //Active When Click ListView
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                myIntentToDetail(position);
+            }
+
+            private void myIntentToDetail(int intClick) {
+                Intent objIntent = new Intent(MainActivity.this,ShowDetailActivity.class);
+                objIntent.putExtra("click",intClick);
+                startActivity(objIntent);
+            }
+        });
+
+
+
+
     }   //Create ListView
 
     public void clickAboutMe(View view) {
