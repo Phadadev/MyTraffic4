02ef4d18ac8d12fb3ce74bcac157618786172f39
NYR <paphat.dev@gmail.com>
2016-01-21 13:41:18 +0700

    Add link About Me button

--- app/src/main/java/com/pw/paphat/mytraffic4/MainActivity.java
+++ app/src/main/java/com/pw/paphat/mytraffic4/MainActivity.java
@@ -1,7 +1,10 @@
 package com.pw.paphat.mytraffic4;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   //Main Method
+
+    public void clickAboutMe(View view) {
+
+        Intent objIntent = new Intent(Intent.ACTION_VIEW);
+        objIntent.setData(Uri.parse("http://www.rcsayoungpilot.com"));
+        startActivity(objIntent);
+    }   //Click
+
+
+}   //Main Class
